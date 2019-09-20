a5df4a54c1b503534ec831278943054a38972bb1
WestleyPoon <poonw@uci.edu>
2019-09-20 16:31:07 -0700

    p7 - attempt 2

--- problems/7.js
+++ problems/7.js
@@ -4,11 +4,18 @@
  */
 var reverse = function(x) {
     let result = 0;
-    while(x) {
+    let negative = false;
+    if (x<0) {
+        negative = true;
+    }
+    while (x) {
         let end = x%10;
         result *= 10;
         result += end;
         x = Math.floor(x/10);
+    }
+    if (negative) {
+        return result * -1;
     }
     return result;
 };
