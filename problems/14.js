6b62ef80aa7a217ad2715f5519cb4a5e6c25b65b
WestleyPoon <poonw@uci.edu>
2019-09-25 15:19:57 -0700

    p14 attempt 1

--- problems/14.js
+++ problems/14.js
@@ -3,7 +3,22 @@
  * @returns {string}
  */
 var longestCommonPrefix = function(strs) {
-    return '';
+    let match = true;
+    const firstChar = strs[0][0];
+    for (let i = 1; i < strs.length; i++) {
+        if (strs[i] === "" || firstChar !== strs[i][0]) {
+            match = false;
+            break;
+        }
+    }
+
+    if (match) {
+        return `${firstChar}` + longestCommonPrefix(strs.map((str) => {
+            return str.slice(1);
+        }))
+    } else {
+        return '';
+    }
 };
 
 console.log(longestCommonPrefix(["flower","flow","flight"]));
