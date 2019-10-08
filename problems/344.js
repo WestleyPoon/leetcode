f660b7a7ee12c1eb04ce2564085c02348bf2b268
WestleyPoon <poonw@uci.edu>
2019-10-08 16:08:03 -0700

    p344 started

---
+++ problems/344.js
@@ -0,0 +1,10 @@
+/**
+ * @param {char[]} s
+ * @returns {void} Do not return anything, modify s in-place instead
+ */
+var reverseString = function(s) {
+    return null;
+};
+
+console.log(reverseString(["h","e","l","l","o"]));
+console.log(reverseString(["h","a","n","n","a","H"]));
