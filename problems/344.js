5e12e75bec57237acffa10f1b364756c1efe9249
WestleyPoon <poonw@uci.edu>
2019-10-09 12:32:28 -0700

    p344 finished

--- problems/344.js
+++ problems/344.js
@@ -3,7 +3,12 @@
  * @returns {void} Do not return anything, modify s in-place instead
  */
 var reverseString = function(s) {
-    return null;
+    let limit = Math.floor(s.length/2);
+    for (let i = 0; i < limit; i++) {
+        let temp = s[i];
+        s[i] = s[s.length - i - 1];
+        s[s.length - i - 1] = temp;
+    }
 };
 
 console.log(reverseString(["h","e","l","l","o"]));
